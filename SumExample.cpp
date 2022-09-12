1614684a2fead19fecaa1b6c8550f3f504373f63
DevangRaval1 <draval@mail.csuchico.edu>
2022-09-12 11:44:25 -0700

    Example Commit

---
+++ SumExample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int sum = 0;
+    for(int i=0;i<10;i++){
+        sum +=i;
+    }
+    cout << "The sum of first 9 numbers is " << sum << endl;
+
+
+    //algorithm
+    
+    
+    return 0;
+}
